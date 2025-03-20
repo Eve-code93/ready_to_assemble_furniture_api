ddc6ad87314e37e0355f8cb9633fe1f61218227b
Eve-code93 <evelynejose1993@gmail.com>
2025-03-20 18:55:30 +0300

    Configured PostgreSQL database and fixed migration issues

--- furniture_api/furniture_api/settings.py
+++ furniture_api/furniture_api/settings.py
@@ -77,23 +77,25 @@ WSGI_APPLICATION = 'furniture_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
      
    #postgres database
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "your_db_name",
-        "USER": "your_db_user",
-        "PASSWORD": "your_db_password",
-        "HOST": "localhost",
-        "PORT": "5432",
+   
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'furniture_db',
+        'USER': 'furniture_user',
+        'PASSWORD': 'mutijose97!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
 
-}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
